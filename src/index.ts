f027838ab1e69c875c1f132271a58e783d5d2b4b
Lewis Chan <8966669+lwschan@users.noreply.github.com>
2024-02-04 23:38:55 +0800

    update telegram-weather-bot

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { configure as configureEnv } from './environment';
 import bot from './bot';
 
+import { setDefaultAutoSelectFamily } from 'net';
+
 configureEnv();
+
+if (setDefaultAutoSelectFamily) {
+  setDefaultAutoSelectFamily(false);
+}
 
 await bot();
